4de0e5cb79c18a1859fb437a7a6cd5e480f04770
Anthony DiRusso <ossurid@gmail.com>
2019-03-29 11:28:11 -0400

    adds models/coffee.js

---
+++ models/coffee.js
@@ -0,0 +1,40 @@
+const db = require('./conn');
+
+
+class Coffee {
+    constructor(id, name, order, size) {
+        this.id = id;
+        this.name = name;
+        this.order = order;
+        this.size = size;
+    }
+
+    static getById(id) {
+        return db.one(`select * from coffee where id=${id}`)
+            .then((coffeeData) => {
+                const coffeeInstance = new Coffee(coffeeData.id,
+                                                  coffeeData.name,
+                                                  coffeeData.order,
+                                                  coffeeData.order,
+                                                  coffeeData.size
+                                                  );
+                return coffeeInstance;
+            })
+            .catch(() => {
+                return null;
+            })
+
+    }
+
+    save() {
+        return db.result(`
+        update coffee set
+        name=${this.name},
+        order=${this.order},
+        status=${this.size},
+    where id=${this.id}
+        `)
+    }
+}
+
+module.exports = Coffee;
