782ed3d9d43631abc56434899f76bc07790967ea
Anthony DiRusso <ossurid@gmail.com>
2019-03-29 09:05:57 -0400

    creates starter files for coffee-orders-app

---
+++ schema.sql
@@ -0,0 +1,6 @@
+create table coffee-orders (
+    id serial primary key,
+    name varchar(200),
+    order varchar(200),
+    size varchar(200)
+);
---
+++ seed.sql
@@ -0,0 +1,8 @@
+insert into coffee-orders
+        (name, order, size)
+values
+    ('Amy', 'Coffee Black', 'grande'),
+    ('Andy', 'Americano Extra Cream Extra Sugar', 'venti'),
+    ('Aretha', 'Cappuccino Light Foam', 'tall'),
+    ('Alex', 'Breve Latte Extra Sugar', 'extra grande')
+;
